4e0081bc0cf72efba570d9c368c8de48ab004300
Adam Sadler <adambsadler@gmail.com>
2022-01-06 21:29:34 -0500

    Added Transaction Create, Edit, and Delete Functionality

---
+++ GeneralStore.MVC/Controllers/TransactionController.cs
@@ -0,0 +1,79 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        // GET: Transaction
+        public ActionResult Index()
+        {
+            List<Transaction> transactionList = _db.Transactions.ToList();
+            List<Transaction> orderedList = transactionList.OrderBy(trans => trans.DateOfTransaction).ToList();
+            return View(orderedList);
+        }
+
+        // GET: Transaction
+        public ActionResult Create()
+        {
+            ViewBag.CustomerID = new SelectList(_db.Customers, "CustomerID", "FullName");
+            ViewBag.ProductID = new SelectList(_db.Products, "ProductId", "Name");
+            return View();
+        }
+
+        // POST: Create
+        // Transaction/Create
+        [HttpPost]
+        public ActionResult Create(Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                Product product = _db.Products.Find(transaction.ProductID);
+                if(transaction.ItemCount <= product.InventoryCount)
+                {
+                    _db.Transactions.Add(transaction);
+                    product.InventoryCount -= transaction.ItemCount;
+                    _db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            // Still need to figure out how to tell the user that there isn't enough inventory
+            return View(transaction);
+        }
+
+        // GET: Delete
+        // Transaction/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Transaction transaction = _db.Transactions.Find(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaction);
+        }
+
+        // POST: Delete
+        // Transaction/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Transaction transaction = _db.Transactions.Find(id);
+            Product product = _db.Products.Find(transaction.ProductID);
+            _db.Transactions.Remove(transaction);
+            product.InventoryCount += transaction.ItemCount;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ GeneralStore.MVC/Models/Transaction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.MVC.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int OrderID { get; set; }
+        [Required]
+        [ForeignKey(nameof(Customer))]
+        [Display(Name = "Customer Name")]
+        public int CustomerID { get; set; }
+        public virtual Customer Customer { get; set; }
+        [Required]
+        [ForeignKey(nameof(Product))]
+        [Display(Name = "Product")]
+        public int ProductID { get; set; }
+        public virtual Product Product { get; set; }
+        [Required]
+        [Display(Name = "Quantity")]
+        // [CustomValidation(typeof(ValidationResult), "ValidateInventory")]
+        public int ItemCount { get; set; }
+        [Required]
+        [Display(Name = "Transaction Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateOfTransaction { get; set; }
+
+        //public ValidationResult ValidateInventory(int productOrdered)
+        //{
+        //    if (productOrdered > Product.InventoryCount)
+        //    {
+        //        return new ValidationResult("There is not enough inventory for this order.");
+        //    }
+
+        //    return ValidationResult.Success;
+        //}
+    }
+}
